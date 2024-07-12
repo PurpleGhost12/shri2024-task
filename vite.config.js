f478c2401e5a0482fbb72eeeb9b30f94296949ec
Anastasiia Cheperegina <cheperegina_as@mail.ru>
2024-07-12 20:22:25 +0300

    vite config

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -7,5 +8,11 @@ export default defineConfig({
   build: {
     outDir: './docs',
     emptyOutDir: true, // also necessary
-  }
+  },
+  resolve: {
+    alias: {
+      // Resolve all .svg files to the @/assets/svgs directory at build time
+      '@assets': path.resolve(__dirname, './src/assets'), 
+    },
+  },
 })
